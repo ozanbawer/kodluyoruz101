8acf3dbffcee86289512807afd9e54f5dfb00c24
OzanBawer <ozanb@aof.anadolu.edu.tr>
2025-04-18 00:51:44 +0300

    update

--- JavaScriptPatika/main.js
+++ JavaScriptPatika/main.js
@@ -1,13 +1,4 @@
 
-let counter =0;
-
-let counterDOM = document.querySelector('#counter');
-let increase = document.querySelector('#increase');
-let discrease = document.querySelector('#discrease');
-
-function increase(click){
-    counter ++
-}
 
 
 
