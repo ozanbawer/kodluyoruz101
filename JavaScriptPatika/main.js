ec28e0b862bb253f856a445edde8b712d4b42daf
OzanBawer <ozanb@aof.anadolu.edu.tr>
2025-04-18 00:50:00 +0300

    ödev1 eklendi

---
+++ JavaScriptPatika/main.js
@@ -0,0 +1,14 @@
+
+let counter =0;
+
+let counterDOM = document.querySelector('#counter');
+let increase = document.querySelector('#increase');
+let discrease = document.querySelector('#discrease');
+
+function increase(click){
+    counter ++
+}
+
+
+
+
